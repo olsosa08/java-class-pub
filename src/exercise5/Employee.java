9189963202d444ee09709673dfa628b5f10737ca
olsosa08 <32493969+olsosa08@users.noreply.github.com>
2017-11-08 16:36:20 -0600

    Part of Ex 5

--- src/exercise5/Employee.java
+++ src/exercise5/Employee.java
@@ -6,5 +6,43 @@ package exercise5;
  * @author yasiro01
  */
 public class Employee {
-
+    private String firstName;
+    private String lastName;
+    private double salary;
+    private boolean fullTime;
+    
+    public Employee(String firstName, String lastName, double salary, boolean fullTime){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.fullTime = fullTime;
+    }
+    
+    public String getFirstName(){
+        return firstName;
+    }
+    
+    public String getLastName(){
+        return lastName;
+    }
+    
+    public double getSalary(){
+        return salary;
+    }
+    
+    public boolean isFullTime(){
+        return fullTime;
+    }
+    
+    @Override
+    public String toString(){
+        String ans = "";
+        if(fullTime){
+            ans = String.format("%s %s earns $%.2f annually", firstName, lastName, salary);
+        }
+        else{
+            ans = String.format("%s %s earns $%.2f hourly", firstName, lastName, salary);
+        }
+        return ans;
+    }
 }
