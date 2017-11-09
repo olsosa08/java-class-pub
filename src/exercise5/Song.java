49538cfd467259ffac8c4980a9ed6b022a65c999
olsosa08 <32493969+olsosa08@users.noreply.github.com>
2017-11-09 14:10:48 -0600

    Part of Ex 5

--- src/exercise5/Song.java
+++ src/exercise5/Song.java
@@ -1,5 +1,7 @@
 package exercise5;
 
+import java.util.Objects;
+
 /**
  * Class Song
  * Task 7: Create a class Song with the following private data members: String title, String artist.
@@ -8,5 +10,62 @@ package exercise5;
  * @author yasiro01
  */
 public class Song {
-  
+    private String title;
+    private String artist;
+    
+    public Song(String title, String artist){
+        this.artist = artist;
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Song other = (Song) obj;
+        if (!Objects.equals(this.title, other.title)) {
+            return false;
+        }
+        if (!Objects.equals(this.artist, other.artist)) {
+            return false;
+        }
+        return true;
+    }
+    
+    @Override
+    public int hashCode() {
+        int accu = 0;
+        for(int i = 0; i < title.length(); i++){
+            char temp = title.charAt(i);
+            accu = accu + (i*temp);
+        }
+        for(int j = 0; j < artist.length(); j++){
+            char temp2 = artist.charAt(j);
+            accu = accu + (j*temp2);
+    }
+        return accu;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("%s by %s", title, artist);
+    }
+    
+    
 }
