aa7770c5e7ee5b3c1ffbc9a52294b6dea1ff1707
olsosa08 <32493969+olsosa08@users.noreply.github.com>
2017-11-09 12:36:55 -0600

    Part of Ex 5

--- src/exercise5/Laptop.java
+++ src/exercise5/Laptop.java
@@ -6,5 +6,40 @@ package exercise5;
  * @author yasiro01
  */
 public class Laptop {
-  
+    private String manufacturer;
+    private double price;
+    private String color;
+    
+    public Laptop(String manufacturer, double price, String color){
+        this.manufacturer = manufacturer;
+        this.color = color;
+        this.price = price;
+    }
+    
+    public String getManufacturer(){
+        return manufacturer;
+    }
+    
+    public String getColor(){
+        return color;
+    }
+    
+    public double getPrice(){
+        return price;
+    }
+    
+    public double setPrice(double newPrice){
+        price = newPrice;
+        return price;
+    }
+    
+    public String setColor(String newColor){
+        color = newColor;
+        return color;
+    }
+    
+    @Override
+    public String toString(){
+        return String.format("%s laptop by %s costs $%.2f", color, manufacturer, price);
+    }
 }
