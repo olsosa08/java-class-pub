bbf94de5931bd699a859783286ab733f237d8ff9
olsosa08 <32493969+olsosa08@users.noreply.github.com>
2017-11-15 22:43:42 -0600

    Finished Ex 6

---
+++ src/exercise7/HourlyEmployee.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise7;
+
+/**
+ *
+ * @author shols
+ */
+public class HourlyEmployee extends Employee {
+    private double wageRate;
+    private double hours;
+
+    public HourlyEmployee(String name, String ssn, double wageRate, double hours) {
+        super(name, ssn);
+        this.wageRate = wageRate;
+        this.hours = hours;
+    }
+
+    public double getWageRate() {
+        return wageRate;
+    }
+
+    public double getHours() {
+        return hours;
+    }
+
+    public void setWageRate(double wageRate) {
+        this.wageRate = wageRate;
+    }
+
+    public void setHours(double hours) {
+        this.hours = hours;
+    }
+
+    @Override
+    public double getNetPay(){
+        return hours * wageRate;
+    }
+    
+    @Override
+    public void printWeeklyCheck(){
+        
+    }
+}
