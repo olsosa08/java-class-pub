86d0a30b1c1ee89d699174f529dd2d8e040ca34a
olsosa08 <32493969+olsosa08@users.noreply.github.com>
2017-11-23 13:10:38 -0600

    Completed Ex 9

--- src/exercise9/Ex9Converter.java
+++ src/exercise9/Ex9Converter.java
@@ -6,26 +6,26 @@ package exercise9;
  */
 public class Ex9Converter {
   public Double c2f(Double valueFrom) {
-    throw new UnsupportedOperationException();
+    return (valueFrom * (9.0/5.0))  +32;
   }
 
   public Double c2k(Double valueFrom) {
-    throw new UnsupportedOperationException();
+    return valueFrom + 273.15;
   }
 
   public Double f2c(Double valueFrom) {
-    throw new UnsupportedOperationException();
+    return (valueFrom - 32) * (5.0/9.0);
   }
 
   public Double f2k(Double valueFrom) {
-    throw new UnsupportedOperationException();
+    return ((valueFrom - 32) * (5.0/9.0))  + 273.15;
   }
 
   public Double k2c(Double valueFrom) {
-    throw new UnsupportedOperationException();
+    return valueFrom - 273.15;
   }
 
   public Double k2f(Double valueFrom) {
-    throw new UnsupportedOperationException();
+    return (valueFrom - 273.15) * (9.0/5.0) + 32;
   }
 }
