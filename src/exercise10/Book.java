28b846d9cb85b536dc5235ecece7fe4063d71290
olsosa08 <32493969+olsosa08@users.noreply.github.com>
2017-11-28 13:37:54 -0600

    Part of Ex10

--- src/exercise10/Book.java
+++ src/exercise10/Book.java
@@ -1,13 +1,93 @@
 package exercise10;
 
+import java.util.Objects;
+
 /**
  * Class Book
  * @author yasiro01
  */
 public class Book {
+    final String title;
+    final String author;
+    Double price;
+    final int year;
+
+    public Book(String title, String author, Double price, int year) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.year = year;
+    }
+    
+    public Book(Book other){
+        this.title = other.title;
+        this.author = other.author;
+        this.price = other.price;
+        this.year = other.year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 67 * hash + Objects.hashCode(this.title);
+        hash = 67 * hash + Objects.hashCode(this.author);
+        hash = 67 * hash + Objects.hashCode(this.price);
+        hash = 67 * hash + this.year;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Book other = (Book) obj;
+        if (this.year != other.year) {
+            return false;
+        }
+        if (!Objects.equals(this.title, other.title)) {
+            return false;
+        }
+        if (!Objects.equals(this.author, other.author)) {
+            return false;
+        }
+        return true;
+    }
+    
+    @Override
+    public String toString(){
+        return String.format("%s (%d) by %s costs $%.2f", title, year, author, price);
+    }
 }
 
 class ByTitle implements Comparator<Book> {
+    
 }
 
 class ByAuthor implements Comparator<Book> {
