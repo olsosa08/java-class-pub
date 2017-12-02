293b8d43b262b4176bf60d430b956e38c1a9e2c4
olsosa08 <32493969+olsosa08@users.noreply.github.com>
2017-12-02 12:08:00 -0600

    Finished EX 11

--- src/exercise11/Ex11Logic.java
+++ src/exercise11/Ex11Logic.java
@@ -1,6 +1,7 @@
 package exercise11;
 
 import java.util.Collection;
+import java.util.Collections;
 
 /**
  * Exercise 11 logic
@@ -15,6 +16,10 @@ public class Ex11Logic {
    * @return length of the parameter's string representation
    */
   // TODO: Implement method strLen
+    public <T> int strLen(T item){
+        String temp = item.toString();
+        return temp.length();
+    }
   
   /**
    * Task 2
@@ -25,7 +30,16 @@ public class Ex11Logic {
    * @return the greater of three parameters
    */
   // TODO: Implement method greater
-  
+    public <T extends Comparable> T greater(T item1, T item2, T item3){
+        if(item1.compareTo(item2) >= 0 && item1.compareTo(item3) >= 0){
+            return item1;
+        } else if(item2.compareTo(item1) >= 0 && item2.compareTo(item3) >= 0){
+            return item2;
+        } else if(item3.compareTo(item1) >= 0 && item3.compareTo(item2) >= 0){
+            return item3;
+        }
+        return null;
+    }
   /**
    * Task 3
    * @param <T>
@@ -33,6 +47,9 @@ public class Ex11Logic {
    * print a list of items
    */
   // TODO: Implement method printList
+    public <T> void printList(T list){
+        System.out.print(list.toString()+ " ");
+    }
   
   /**
    * Task 4
@@ -41,6 +58,18 @@ public class Ex11Logic {
    * @return the smallest item in the collection
    */
   // TODO: Implement method findMinItem
+    public <T extends Comparable> T findMinItem(Collection<T> list){
+       T min = null;
+       for (T item: list){
+           if(min == null){
+               min = item;
+           }
+           if(item.compareTo(min) < 0){
+               min = item;
+           }
+       }
+       return min;
+    }
 }
 
   /**
@@ -51,3 +80,35 @@ public class Ex11Logic {
    * Implement the constructor, getters, setters, and toString
    */
   // TODO: Implement class Foo
+class Foo<T, U> {
+    public T fuzz;
+    public U buzz;
+
+    public Foo(T fuzz, U buzz) {
+        this.fuzz = fuzz;
+        this.buzz = buzz;
+    }
+
+    public T getFuzz() {
+        return fuzz;
+    }
+
+    public U getBuzz() {
+        return buzz;
+    }
+
+    public void setFuzz(T fuzz) {
+        this.fuzz = fuzz;
+    }
+
+    public void setBuzz(U buzz) {
+        this.buzz = buzz;
+    }
+
+    @Override
+    public String toString() {
+        return "Foo{" + "fuzz=" + fuzz + ", buzz=" + buzz + '}';
+    }
+    
+    
+}
